3cdcc3374f253279a7a43568604882a4c8ca0bfd
Divakar J <138748411+divakar9626@users.noreply.github.com>
2024-07-12 23:51:32 +0530

    Update Ch3_pgm_12.js

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -17,3 +17,25 @@ var post = {
  * 2) Add a second post.
  *
  */
+
+var post = {
+	id : 1,
+	title : "My Crazy Space Adventure",
+	author : "Philae",
+	created : "2015-06-21",
+	body : "You will not believe where I just woke up!! Only on a comet..."
+};
+
+
+console.log("title "  + post.title);
+console.log("author name is " + post.author + "\n");
+
+var post1 = {
+	id : 2,
+	title : "just do it",
+	company : "nike",
+	created : "1997",
+	body : "what's the worst thing could be happen JUST DO IT"
+}; 
+
+console.log(post1.body + "\n")
