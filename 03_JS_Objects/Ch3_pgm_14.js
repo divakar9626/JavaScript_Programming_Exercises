ba7847517bca09e3eaeffe97e01233c03aa549bd
Divakar J <138748411+divakar9626@users.noreply.github.com>
2024-07-14 17:52:56 +0530

    Update Ch3_pgm_14.js

--- 03_JS_Objects/Ch3_pgm_14.js
+++ 03_JS_Objects/Ch3_pgm_14.js
@@ -18,3 +18,19 @@ var questionAndAnswer = {
  *    and answer options in a nicely formatted way.
  *
  */
+
+
+var questionAndAnswer = {
+	question: "What is the capital of France?",
+	answer1: "Bordeaux",
+	answer2: "F",
+	answer3: "Paris",
+	answer4: "Brussels",
+	correctAnswer: "Paris",
+	marksForQuestion: 2
+};
+
+
+console.log(questionAndAnswer.question + "\n" + questionAndAnswer.answer1 + "\n" + questionAndAnswer.answer2 + "\n" + questionAndAnswer.answer4);
+console.log("marks for the question is " + questionAndAnswer.marksForQuestion);
+console.log("\n" +  "the correct answer is " + questionAndAnswer.correctAnswer);
