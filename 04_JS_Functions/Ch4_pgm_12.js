fb281353fb79f6591eb2cbfc4c2a1e37309c50a6
Divakar J <138748411+divakar9626@users.noreply.github.com>
2024-07-14 19:15:06 +0530

    Update Ch4_pgm_12.js

--- 04_JS_Functions/Ch4_pgm_12.js
+++ 04_JS_Functions/Ch4_pgm_12.js
@@ -52,3 +52,41 @@ displaySale();
  *    have to call both functions for every sale?
  *
  */
+
+var sale1;
+var sale2;
+var sale3;
+var sale4;
+var sale;
+
+var displaySale;
+
+sale1 = { price: 140, taxRate: 15 };
+sale2 = { price: 40, taxRate: 10 };
+sale3 = { price: 120, taxRate: 20 };
+sale4 = { price: 180, taxRate: 18 };
+
+
+
+displaySale = function () {
+	sale.tax = sale.price * sale.taxRate / 100;
+	sale.total = sale.price + sale.tax;
+
+  console.log("\nprice = $" + sale.price);
+  console.log("tax @ " + sale.taxRate + "% = $" + sale.tax);
+  console.log("total cost = $" + sale.total + "\n");
+};
+
+sale = sale1;
+
+displaySale();
+
+sale = sale2;
+
+displaySale();
+
+sale = sale3;
+displaySale();
+
+sale = sale4;
+displaySale();
