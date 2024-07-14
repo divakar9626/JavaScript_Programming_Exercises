db86720fef0088868cdeb3ab5cb5849a1229f085
Divakar J <138748411+divakar9626@users.noreply.github.com>
2024-07-14 18:44:27 +0530

    Update Ch4_pgm_05.js

--- 04_JS_Functions/Ch4_pgm_05.js
+++ 04_JS_Functions/Ch4_pgm_05.js
@@ -31,3 +31,25 @@ displayMenu = function () {
  * 5) Run the displayMenu function.
  *
  */
+
+var findTotal;
+var displayMenu;
+
+findTotal = function () {
+	var number1 = 100;
+	var number2 = 100;
+	var result;
+
+	result = number1 + number2;
+	console.log(result)
+};
+
+displayMenu = function () {
+	console.log("Please choose an option:");
+	console.log("(1) Print log");
+	console.log("(2) Upload file");
+	console.log("(9) Quit");
+};
+
+findTotal();
+displayMenu();
