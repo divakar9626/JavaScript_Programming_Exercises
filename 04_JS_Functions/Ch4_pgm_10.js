5efe7083ed5a1e69e73bb1febe0e7653d1e98043
Divakar J <138748411+divakar9626@users.noreply.github.com>
2024-07-14 18:59:07 +0530

    Update Ch4_pgm_10.js

--- 04_JS_Functions/Ch4_pgm_10.js
+++ 04_JS_Functions/Ch4_pgm_10.js
@@ -41,3 +41,30 @@ showMovieInfo();
  *    What happens when you call showMovieInfo now?
  *
  */
+
+// it will run
+
+var movie2;
+var showMovieInfo;
+var movie;
+
+
+movie2 = {
+    title: "Inside Out",
+    actors: "Amy Poehler, Bill Hader",
+    directors: "Pete Doctor, Ronaldo Del Carmen"
+};
+
+showMovieInfo = function () {
+  console.log("Movie information for " + movie.title);
+  console.log("------------------------------");
+  console.log("Actors: " + movie.actors);
+  console.log("Directors: " + movie.directors);
+  console.log("------------------------------");
+};
+
+
+
+movie = movie2;
+
+showMovieInfo();
