2c27c8789ca51ae7f10825b492c885a1d63c6ecc
Divakar J <138748411+divakar9626@users.noreply.github.com>
2024-07-14 18:12:56 +0530

    Update Ch4_pgm_02.js

--- 04_JS_Functions/Ch4_pgm_02.js
+++ 04_JS_Functions/Ch4_pgm_02.js
@@ -54,3 +54,46 @@ console.log("------------------------------");
  * 4) Display info from the three events on the console.
  *
  */
+
+
+var movie1;
+var movie2;
+var movie3;
+var movie4;
+
+movie1 = {
+  title: "Inside Out",
+  actors: "Amy Poehler, Bill Hader",
+  directors: "Pete Doctor, Ronaldo Del Carmen"
+};
+
+movie2 = {
+  title: "Spectre",
+  actors: "Daniel Craig, Christoph Waltz",
+  directors: "Sam Mendes"
+};
+
+movie3 = {
+  title: "Star Wars: Episode VII - The Force Awakens",
+  actors: "Harrison Ford, Mark Hamill, Carrie Fisher",
+  directors: "J.J.Abrams"
+};
+
+movie4 = {
+  title: "Despicable me 4",
+  actors: "Pierre Coffin, Steve Carell",
+  directors: "Chris Renaud, Patrick Delage"
+};
+
+console.log("Movie information for " + movie1.title + "\n" +  "------------------------------\n" + "Actors: " + movie1.actors + "\n" + 
+	"Directors: " + movie1.directors + "\n"  +  "------------------------------------------------------------------------------------------------------------------------\n\n\n" +
+	
+	"Movie information for " + movie2.title + "\n" +  "------------------------------\n" + "Actors: " + movie2.actors + "\n" + 
+	"Directors: " + movie2.directors + "\n"  +  "------------------------------------------------------------------------------------------------------------------------\n\n\n" +
+
+	"Movie information for " + movie3.title + "\n" +  "------------------------------\n" + "Actors: " + movie3.actors + "\n" + 
+	"Directors: " + movie3.directors + "\n"  +  "------------------------------------------------------------------------------------------------------------------------\n\n\n" + 
+
+	"Movie information for " + movie4.title + "\n" +  "------------------------------\n" + "Actors: " + movie4.actors + "\n" + 
+	"Directors: " + movie4.directors + "\n"  +  "------------------------------------------------------------------------------------------------------------------------\n\n\n" );
+
